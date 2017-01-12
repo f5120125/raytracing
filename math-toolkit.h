8c07f38fff501685408fb1d74d9905418067e7a0
f5120125 <f5120125@gmail.com>
2017-01-12 23:03:18 +0800

    oringinal_version

--- math-toolkit.h
+++ math-toolkit.h
@@ -22,32 +22,51 @@ double length(const double *v)
     return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
 }
 
-static inline
+static inline __forceinline
 void add_vector(const double *a, const double *b, double *out)
 {
-    for (int i = 0; i < 3; i++)
+    /*for (int i = 0; i < 3; i++)
         out[i] = a[i] + b[i];
+	*/
+	out[0] = a[0] + b[0];
+	out[1] = a[1] + b[1];
+	out[2] = a[2] + b[2];
 }
 
-static inline
+static inline __forceinline
 void subtract_vector(const double *a, const double *b, double *out)
 {
-    for (int i = 0; i < 3; i++)
+    /*for (int i = 0; i < 3; i++)
         out[i] = a[i] - b[i];
+	*/
+	
+	out[0] = a[0] - b[0];
+	out[1] = a[1] - b[1];
+	out[2] = a[2] - b[2];
 }
 
-static inline
+static inline __forceinline
 void multiply_vectors(const double *a, const double *b, double *out)
 {
-    for (int i = 0; i < 3; i++)
+    /*for (int i = 0; i < 3; i++)
         out[i] = a[i] * b[i];
+	*/
+	
+	out[0] = a[0] * b[0];
+	out[1] = a[1] * b[1];
+	out[2] = a[2] * b[2];
 }
 
-static inline
+static inline __forceinline
 void multiply_vector(const double *a, double b, double *out)
 {
-    for (int i = 0; i < 3; i++)
+    /*for (int i = 0; i < 3; i++)
         out[i] = a[i] * b;
+	*/
+	
+	out[0] = a[0] * b;
+	out[1] = a[1] * b;
+	out[2] = a[2] * b;
 }
 
 static inline
@@ -58,12 +77,15 @@ void cross_product(const double *v1, const double *v2, double *out)
     out[2] = v1[0] * v2[1] - v1[1] * v2[0];
 }
 
-static inline
+static inline __forceinline
 double dot_product(const double *v1, const double *v2)
 {
     double dp = 0.0;
-    for (int i = 0; i < 3; i++)
-        dp += v1[i] * v2[i];
+    /*for (int i = 0; i < 3; i++)
+        dp += v1[i] * v2[i];*/
+	dp += v1[0] * v2[0];
+	dp += v1[1] * v2[1];
+	dp += v1[2] * v2[2];
     return dp;
 }
 
